d3aa755ac7d885a63c37684124d1091d7600f251
Nishant Rodrigues <nishantjr@gmail.com>
2025-01-06 07:12:40 +0000

    Move customKeybindings to Main

--- src/Config.hs
+++ src/Config.hs
@@ -1,16 +1,13 @@
 module Config where
 
-import Control.Monad (when)
 import Foreign
 import Foreign.C (CUInt)
 import Foreign.C.String
 import System.Environment (getArgs, setEnv)
-import System.Process (spawnProcess)
 
 import qualified LibTinyWLHS.Compositor.Compositor as Compositor
 import LibTinyWLHS.Compositor.Types
 import LibTinyWLHS.KeyBinding.KeyBindings
-import LibTinyWLHS.KeyBinding.KeySyms
 import qualified LibTinyWLHS.Server.FFI as FFI
 import qualified LibTinyWLHS.Server.Server as Server
 import LibTinyWLHS.Server.Types (TinyWLServer)
@@ -21,10 +18,10 @@ import WLR.Util.Log
 
 data Config = Config
     { logLevel :: WLR_log_importance
-    , startupApplication :: String
-    , onStartup :: IO ()
     , modKey :: Modifier
-    , terminalEmulator :: String
+    , onStartup :: IO ()
+    , onKeyPress :: State -> CUInt -> IO ()
+    , startupApplication :: String
     }
 
 data State = State
@@ -41,74 +38,7 @@ terminate state = FFI.c_wl_display_terminate $ display state
 customKeybindings
     :: State -> Config -> IO (FunPtr (CUInt -> IO ()))
 customKeybindings state config = do
-    let
-        handler :: CUInt -> IO ()
-        handler sym = do
-            -- Add your custom key event handler heres
-            wlr_log WLR_INFO $ "Handler called with sym: " ++ show sym
-            when (sym == keySymToInt KEY_s) $ do
-                -- simple match to key events defined in LibTinyWL.KeyBinding.KeySyms
-                wlr_log WLR_INFO "Mod + s pressed, spawning a terminal emulator"
-                -- for this key event a process is spawned in Haskell
-                _ <- spawnProcess (terminalEmulator config) []
-                -- _ <- spawnProcess "swaybg" ["-i", "~/.wallpapers/haskell.png", "-m", "fill"]
-                pure ()
-
-            when (sym == keySymToInt KEY_a) $ do
-                -- simple match to key events defined in LibTinyWL.KeyBinding.KeySyms
-                wlr_log WLR_INFO "Mod + a pressed, running beMenu"
-                _ <-
-                    spawnProcess
-                        "bemenu-run"
-                        [ "-i" -- case insensitive
-                        , "-l"
-                        , "10" -- show 10 lines
-                        , "-p"
-                        , "run:" -- prompt
-                        , "--tb"
-                        , "#285577" -- title background
-                        , "--tf"
-                        , "#ffffff" -- title foreground
-                        , "--fb"
-                        , "#222222" -- filter background
-                        , "--ff"
-                        , "#ffffff" -- filter foreground
-                        , "--nb"
-                        , "#222222" -- normal background
-                        , "--nf"
-                        , "#888888" -- normal foreground
-                        , "--hb"
-                        , "#285577" -- highlighted background
-                        , "--hf"
-                        , "#ffffff" -- highlighted foreground
-                        , "--fn"
-                        , "monospace 12" -- font
-                        , "-W"
-                        , terminalEmulator config ++ " -e"
-                        ] -- for this key event a process is spawned in Haskell
-                pure ()
-
-            when (sym == keySymToInt KEY_c) $ do
-                -- the key Events just show up here as ints so you can also match against a raw int
-                wlr_log WLR_INFO "Mod + c pressed closing server"
-                -- for this event we call a Wayland FFI function
-                terminate state
-                pure ()
-            when
-                ( sym == keySymToInt KEY_d || sym == keySymToInt KEY_v || sym == keySymToInt KEY_l
-                )
-                $ do
-                    -- You can also use logical OR
-                    wlr_log WLR_INFO "Mod + d pressed, cycling windows"
-                    result <- cycleWindows state
-                    ( if result
-                            then wlr_log WLR_INFO "window cycled"
-                            else wlr_log WLR_INFO "Window cycling failed, Only one window"
-                        )
-
-                    pure ()
-
-    mkKeybindingHandler handler
+    mkKeybindingHandler $ (onKeyPress config) state
 
 runWLHS :: Config -> IO ()
 runWLHS config = do
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
+import Control.Monad (when)
+import System.Process (spawnProcess)
+
 import Config
 import LibTinyWLHS.KeyBinding.KeyBindings
+import LibTinyWLHS.KeyBinding.KeySyms
 import Setup
 import WLR.Util.Log
 
@@ -10,14 +14,76 @@ appConfig :: Config
 appConfig = Config
         { logLevel = WLR_DEBUG -- WLR_INFO | WLR_DEBUG | WLR_SILENT | WLR_ERROR
         , startupApplication = "" -- can be any app that works with wayland, Leave blank for no startup app
-        , onStartup = startApps
+        , onStartup = handleStartup
+        , onKeyPress = handleKeyPress
         , modKey = ModAlt -- ModAlt | ModCtrl | ModLogo | ModShift
-        , terminalEmulator = "kitty" -- I use kitty as my emulator, alacritty is also a popular choice
         }
-    where startApps = do startUpProcess [ ("yambar", [])
-                                        , ("swaybg", ["-i", "./images/haskell.png", "-m", "fill"])
-                                        ]
+    where
+          terminalEmulator = "kitty" -- I use kitty as my emulator, alacritty is also a popular choice
+          handleStartup = do startUpProcess [("yambar", [])
+                                            , ("swaybg", ["-i", "./images/haskell.png", "-m", "fill"])
+                                            ]
+          handleKeyPress state sym = do
+            -- Add your custom key event handler heres
+            wlr_log WLR_INFO $ "Handler called with sym: " ++ show sym
+            when (sym == keySymToInt KEY_s) $ do
+                -- simple match to key events defined in LibTinyWL.KeyBinding.KeySyms
+                wlr_log WLR_INFO "Mod + s pressed, spawning a terminal emulator"
+                -- for this key event a process is spawned in Haskell
+                _ <- spawnProcess terminalEmulator []
+                -- _ <- spawnProcess "swaybg" ["-i", "~/.wallpapers/haskell.png", "-m", "fill"]
+                pure ()
+            when (sym == keySymToInt KEY_a) $ do
+                -- simple match to key events defined in LibTinyWL.KeyBinding.KeySyms
+                wlr_log WLR_INFO "Mod + a pressed, running beMenu"
+                _ <-
+                    spawnProcess
+                        "bemenu-run"
+                        [ "-i" -- case insensitive
+                        , "-l"
+                        , "10" -- show 10 lines
+                        , "-p"
+                        , "run:" -- prompt
+                        , "--tb"
+                        , "#285577" -- title background
+                        , "--tf"
+                        , "#ffffff" -- title foreground
+                        , "--fb"
+                        , "#222222" -- filter background
+                        , "--ff"
+                        , "#ffffff" -- filter foreground
+                        , "--nb"
+                        , "#222222" -- normal background
+                        , "--nf"
+                        , "#888888" -- normal foreground
+                        , "--hb"
+                        , "#285577" -- highlighted background
+                        , "--hf"
+                        , "#ffffff" -- highlighted foreground
+                        , "--fn"
+                        , "monospace 12" -- font
+                        , "-W"
+                        , terminalEmulator ++ " -e"
+                        ] -- for this key event a process is spawned in Haskell
+                pure ()
 
+            when (sym == keySymToInt KEY_c) $ do
+                -- the key Events just show up here as ints so you can also match against a raw int
+                wlr_log WLR_INFO "Mod + c pressed closing server"
+                -- for this event we call a Wayland FFI function
+                terminate state
+                pure ()
+            when
+                ( sym == keySymToInt KEY_d || sym == keySymToInt KEY_v || sym == keySymToInt KEY_l
+                )
+                $ do
+                    -- You can also use logical OR
+                    wlr_log WLR_INFO "Mod + d pressed, cycling windows"
+                    result <- cycleWindows state
+                    if result
+                          then wlr_log WLR_INFO "window cycled"
+                          else wlr_log WLR_INFO "Window cycling failed, Only one window"
+                    pure ()
 
 main :: IO ()
 main = runWLHS appConfig
